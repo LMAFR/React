85ef4384b08d053e5095c6cc09331b0f7eab8de9
LMAFR <alejandrofloridoreyes@gmail.com>
2023-03-07 00:03:12 +0000

    Added lazy loading (though is commented as we are getting an unknown error).

--- projects/router-tool/src/App.jsx
+++ projects/router-tool/src/App.jsx
@@ -1,21 +1,27 @@
+import { lazy, Suspense } from 'react'
 import './App.css'
 import { Router } from './Router'
 import Page404 from './pages/404'
-import { HomePage } from './pages/Home'
-import { AboutPage } from './pages/About'
 import SearchPage from './pages/Search'
 import { Route } from './Route'
+import { HomePage } from './pages/Home.jsx'
+import { AboutPage } from './pages/About.jsx'
+
+// Add dynamic import for pages in order to use lazy loading on them
+// const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
+// const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
+
+const appRoutes = [
+  {
+    path: '/search/:query',
+    Component: SearchPage
+  }
+]
 
 function App () {
-  const appRoutes = [
-    {
-      path: '/search/:query',
-      Component: SearchPage
-    }
-  ]
-
   return (
     <main>
+      {/* <Suspense fallback={<div>Loading...</div>}> */}
       <Router
         routes={appRoutes}
         defaultComponent={Page404}
@@ -23,6 +29,7 @@ function App () {
         <Route path='/' Component={HomePage} />
         <Route path='/about' Component={AboutPage} />
       </Router>
+      {/* </Suspense> */}
     </main>
   )
 }
